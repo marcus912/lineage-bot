8224f4d9d7bb839026814dbe805b4a145e901b2e
Marcus Lin <marcus.lin@linksys.com>
2024-09-25 17:56:39 +0800

    Add two files

---
+++ lin-bot/capture-pics.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+from time import time
+from windowcapture import WindowCapture
+
+wincap = WindowCapture()
+loop_time = time()
+while(True):
+    screenshot = wincap.get_screenshot()
+
+
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        cv.destroyAllWindows()
+        break
+    elif key == ord('f'):
+        cv.imwrite('positive/{}.png'.format(loop_time), screenshot)
+    elif key == ord('d'):
+        cv.imwrite('negative/{}.png'.format(loop_time), screenshot)
