7b3f1218c149295af4a2c99a430ab1fb1884d43c
Marcus Lin <waterbalance2000@gmail.com>
2024-10-01 01:17:53 +0800

    fix capture.bat

--- bin/capture.bat
+++ bin/capture.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
-
+cd ../
 echo Check Python venv
-if not exist "venv" (
+if not exist ".venv" (
     echo folder not exists
-    python -m venv venv
+    python -m venv .venv
 )
 
 echo install required python libs
-.\venv\Scripts\activate && pip install -r requirements.txt && python lin_window_capture\capture-screen.py
+.\.venv\Scripts\activate && pip install -r requirements.txt && python lin_window_capture\capture-screen.py
 
 pause
