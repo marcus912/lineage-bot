eb49191a2b9eb29290c3a42d828a906886898ac0
Marcus Lin <waterbalance2000@gmail.com>
2024-11-13 15:25:47 +0800

    add chi2 bat

---
+++ bin/chi2.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+SET VAR_NAME=%LIN_BOT_PATH%
+
+IF "%VAR_NAME%"=="" (
+    echo Environment variableLIN_BOT_PATH is not set.
+    exit /b 1
+) ELSE (
+    echo Environment variable LIN_BOT_PATH is set to %VAR_NAME%.
+)
+
+cd %LIN_BOT_PATH%
+echo Check Python venv
+if not exist ".venv" (
+    echo folder not exists, create venv
+    python -m venv .venv
+)
+
+echo install required python libs
+.\.venv\Scripts\activate && pip install -r requirements.txt && python lin_bot\auto_attack_chi2_v1.py
+
+pause
