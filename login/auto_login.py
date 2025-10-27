c87e7681411f50df0d83c7eaef02f036fae4feb8
Marcus <marcus.lin@linksys.com>
2025-10-27 22:29:35 +0800

    Run the while loop

--- login/auto_login.py
+++ login/auto_login.py
@@ -2,8 +2,12 @@ from time import sleep
 
 import pyautogui
 
-
 def login():
     print('Start a login process')
     #
-    pyautogui.moveTo()
+    pyautogui.moveTo(500, 400)
+    pyautogui.moveTo(400, 500)
+
+while True:
+    login()
+    sleep(120)
